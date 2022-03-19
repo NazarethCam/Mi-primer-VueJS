3cdf5b975dd7f07aafcc16e40e53292841800a0e
NazarethCam <naza3192@gmail.com>
2022-03-19 13:06:44 -0300

    Mi primer Vuejs

---
+++ main.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    lista: [
+      {
+        nombre: "Carlos",
+        apellido: "Lopez",
+        run: "10400132 - K",
+        nacimiento: "12-05-67",
+        edad: 54
+      },
+      {
+        nombre: "Marta",
+        apellido: "Leal",
+        run: "19827384 - 0",
+        nacimiento: "30-11-92",
+        edad: 29
+      },
+      {
+        nombre: "Fernanda",
+        apellido: "Cisneros",
+        run: "13718647 - 1",
+        nacimiento: "02-12-55",
+        edad:66 
+      },
+      {
+        nombre: "Pablo",
+        apellido: "Peña",
+        run: "18775491 - 5",
+        nacimiento: "16-10-79",
+        edad: 42
+      }
+    ]
+  }
+});
